e0bd4692c3552fa2d3f1b4b4dce793bbee892d70
magiclu550 <843983728@qq.com>
2019-07-13 07:34:45 +0800

    [proxy] Object to responseValue

--- jsmod2/JsonSetting.cs
+++ jsmod2/JsonSetting.cs
@@ -5,15 +5,15 @@ namespace jsmod2
     //设置一个协议对象
     public class JsonSetting
     {
-        public JsonSetting(int id, object o, IdMapping idMapping)
+        public JsonSetting(int id, object responseValue, IdMapping idMapping)
         {
             this.id = id;
-            Object = o;
+            this.responseValue = responseValue;
             this.idMapping = idMapping;
         }
 
         public int id { get; }
-        public Object Object { get; }
+        public Object responseValue { get; }
 
         public IdMapping idMapping { get; }
 
