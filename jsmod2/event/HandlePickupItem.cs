efcb35dddba7d8ca57149ba056753358bdd1b9dc
magiclu550 <843983728@qq.com>
2019-08-01 09:03:56 +0800

    [proxy] fix packet bug

--- jsmod2/event/HandlePickupItem.cs
+++ jsmod2/event/HandlePickupItem.cs
@@ -9,10 +9,10 @@ namespace jsmod2
         void IEventHandlerPlayerPickupItem.OnPlayerPickupItem(PlayerPickupItemEvent ev)
         {
             ProxyHandler.handler.sendEventObject(ev,0x31,new IdMapping()
-                .appendId(Lib.ID,ev,Guid.NewGuid().ToString())
-                .appendId(Lib.PLAYER_ID,ev.Player,Guid.NewGuid().ToString())
-                .appendId(Lib.PLAYER_EVENT_SCPDATA_ID,ev.Player.Scp079Data,Guid.NewGuid().ToString())
-                .appendId(Lib.ITEM_EVENT_ID,ev.Item,Guid.NewGuid().ToString())
+                .appendId(Lib.ID,Guid.NewGuid().ToString(),ev)
+                .appendId(Lib.PLAYER_ID,Guid.NewGuid().ToString(),ev.Player)
+                .appendId(Lib.PLAYER_EVENT_SCPDATA_ID,Guid.NewGuid().ToString(),ev.Player.Scp079Data)
+                .appendId(Lib.ITEM_EVENT_ID,Guid.NewGuid().ToString(),ev.Item)
             );
         }
     }
